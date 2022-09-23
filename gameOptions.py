0ec7f81957d090ef1e666a60f5ea1db1561183b5
samgonz <doomteam1@gmail.com>
2022-09-23 00:19:47 -0500

    commit game options

---
+++ gameOptions.py
@@ -0,0 +1,52 @@
+def leftOption():
+     return '''
+You decide to enter the bright cave on the right,
+you hope that you chose the right way.
+You walk for what seems like hours, you see another two paths.'''
+
+
+def youWin():
+    return '''
+You Enter the yellow door, suddenly you see yourself in a room of gold.
+In the room you see a letter and a ring,
+the sign reads,(he who wears the ring and speaks the following can have all their desires achieved.
+Shika high, shika low, see me once, see me go!
+You deside to place on the ring and you chant,
+shika low, see me once, see me go!
+You find yourself at home with all the gold.
+You win the game, you are rich and you can go anywhere, maybe there will be another adventure?
+Wait and see?
+(wait)'''
+               
+def leftOrRight():
+    return '''
+You find yourself lost in a cave, you see two pathways, 
+a long dark path to the right, possibly going that way is my way out and 
+a bright much deeper cave on your left, maybe its an entrance, lets hope so.
+Do you go (left or right)?'''
+
+def gameOver():
+    return '''
+Unfortunatly you chose the wrong thing, you lose.
+Gameover, done, no more, maybe you should try again?
+Lets hope next time you make better choices.'''
+
+def swimOrWait(): 
+    return '''
+You can either swim to the bottom and hope to find another cave, 
+or wait and take a rest. I mean afterall you are tired. 
+(Swim or Wait)'''
+
+def swimOrRunBack():
+    return '''
+You start to hear noises all around you,
+even though the room is bright you see nothing.
+Do you run back to the entrance, or try to swim?
+(Back or Swim)'''
+
+def redOrBlue():
+    return '''
+You run back towards the beginning,
+suddenly you come across three doors, where did they come from?
+Do you enter the blue, yellow or red door?
+(Blue, yellow or Red)?'''
